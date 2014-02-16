179973fcddd2fa8b23c0e8c0c21f9bd36f6980f5
SnehaR <raghunathan.sneha@gmail.com>
2014-02-15 22:13:53 -0600

    Create sr29967-RunXML.py
    Acceptance Tests

---
+++ sr29967-RunXML.py
@@ -0,0 +1,1019 @@
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<red><green></green></red>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<red><blue></blue></red>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<red><yellow></yellow></red>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<green><blue></blue></green>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<green><yellow></yellow></green>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<green><purple></purple></green>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<green><blue></blue><yellow></yellow></green>
+
+<red>
+    <green>
+        <blue></blue>
+        <yellow></yellow>
+    </green>
+</red>
+<green><yellow></yellow><blue></blue></green>
+
+<oct>
+	<libra>
+		<osha>
+			<lids><lids>
+			<king></king>
+		</osha>
+		<lyrics>
+			<hello></hello>
+		</lyrics>
+	</libra>
+</oct>
+<libra><lyrics></lyrics></libra>
+
+<words>
+	<dice>
+		<cold></cold>
+		<ice></ice>
+	</dice>
+	<dime>
+		<time></time>
+	</dime>
+</words>
+<dice><time></time></dice>
+
+<AAA>
+	<Bbb>
+		<abc></abc>
+	</Bbb>
+	<ccc>
+		<abc></abc>
+		<bbb></bbb>
+	</ccc>
+</AAA>
+<bbb></bbb>
+
+<lego>
+	<never>
+		<say>
+			<never></never>
+		</say>
+		<pollo>
+			<loco></loco>
+		</pollo>
+		<face></face>
+	</never>
+</lego>
+<pollo><loco></loco></pollo>
+
+<case>
+	<test></test>
+</case>
+<case><fail></fail</case>
+
+<liars>
+	<gonna>
+		<lie></lie>
+	</gonna>
+	<haters>
+		<gonna>
+			<hate></hate>
+		</gonna>
+	</haters>
+</liars>
+<liars><gonna></gonna></liars>
+
+<define>
+	<peace>
+		<war></war>
+	</peace>
+</define>
+<define><war></war></define>
+
+<local>
+	<gloves></gloves>
+	<toes>
+		<little></little>
+		<big></big>
+		<fat></fat>
+	</toes>
+	<fat>
+		<faces></faces>
+	</fat>
+</local>
+<fat></fat>
+
+<bros>
+	<before>
+		<hose></hose>
+	</before>
+	<afore>
+		<beaches></beaches>
+	</afore>
+	<after>
+		<me>
+			<bros>
+				<before></before>
+			</bros>
+		</me>
+	</after>
+</bros>
+<bros><before></before></bros>
+
+<butter>
+	<toast>
+		<evil></evil>
+		<maybe></maybe>
+	</toast>
+	<evil></evil>
+	<maybe></maybe>
+</butter>
+<butter><evil><maybe></maybe></evil></butter>
+
+<have>
+	<yourself>
+		<a></a>
+	</yourself>
+	<merry>
+		<little>
+			<christmas></christmas>
+		</little>
+	</merry>
+	<or>
+		<have>
+			<yourself></yourself>
+		</have>
+	</or>
+</have>
+<have><yourself></yourself></have>
+
+<fu>
+	<man></man>
+	<chu>
+		<man>
+			<chu></chu>
+		</man>
+	</chu>
+</fu>
+<man><chu></chu></man>
+
+<oreos>
+	<chips></chips>
+	<ahoy></ahoy>
+	<lettuce>
+		<oreos>
+			<chips></chips>
+		</oreos>
+	</lettuce>
+</oreos>
+<oreos><chips></chips></oreos>
+
+<lets>
+	<stay>
+		<together></together>
+	</stay>
+</lets>
+<lets><together></together></lets>
+
+<github>
+	<is>
+		<cool></cool>
+	</is>
+	<or>
+		<is></is>
+		<uncool></uncool>
+	</or>
+	<nuts></nuts>
+</github>
+<github><is></is></github>
+
+<cs>
+	<noncs>
+		<nonsense></nonsense>
+	</noncs>
+</cs>
+<cs><is><nonsense></nonsense></is></cs>
+
+<capital>
+	<capitol>
+		<Capital>
+			<Capitol></Capitol>
+		</Capital>
+	</capitol>
+</capital>
+<Capital><Capitol></Capitol></Capital>
+
+<do>
+	<re>
+		<mi>
+			<fa>
+				<so>
+					<la>
+						<ti>
+							<do></do>
+						</ti>
+					</la>
+				</so>
+			</fa>
+		</mi>
+	</re>
+</do>
+<do></do>
+
+<create>
+	<delete>
+		<destroy>
+			<create>
+				<delete></delete>
+			</create>
+		</destroy>
+	</delete>
+</create>
+<create><delete></delete></create>
+
+<qwerty>
+	<lambda>
+		<loci></loci>
+		<lambda>
+			<loci>
+				<lambda>
+					<loci></loci>
+				</lambda>
+			</loci>
+		</lambda>
+	</lambda>
+</qwerty>
+<lambda><loci></loci></lambda>
+
+<lisa>
+	<liza></liza>
+	<missy></missy>
+	<kermit>
+		<sprinkles></sprinkles>
+	</kermit>
+	<lola>
+		<sprinkle></sprinkle>
+	</lola>
+</lisa>
+<lola><sprinkles></sprinkles></lola>
+
+<graham>
+	<marshmallow>
+		<chocolate></chocolate>
+		<graham></graham>
+		<marshmallow></marshmallow>
+	</marshmallow>
+</graham>
+<graham><marshmallow></marshmallow></graham>
+
+<one>
+	<zero>
+		<one>
+			<zero></zero>
+		</one>
+	</zero>
+	<one>
+		<zero></zero>
+	</one>
+</one>
+<one><zero></zero></one>
+
+<HOPE>
+	<Hope>
+		<hope></hope>
+		<Hope></Hope>
+		<HOPE></HOPE>
+	</Hope>
+	<hope></hope>
+	<HOPE></HOPE>
+</HOPE>
+<Hope><HOPE></HOPE></Hope>
+
+<escape>
+	<the>
+		<fate>
+			<pierce></pierce>
+			<the></the>
+			<veil></veil>
+		</fate>
+		<veil></veil>
+	</the>
+</escape>
+<the><veil></veil></the>
+
+<Karma>
+	<Karla>
+		<Karmen></Karmen>
+		<Karen></Karen>
+		<Caramel></Caramel>
+		<Karla></Karla>
+	</Karla>
+</Karma>
+<Karla></Karla>
+
+<Suppose>
+	<Beliefs>
+		<Profound>
+			<Thoughts>
+				<Beliefs></Beliefs>
+			</Thoughts>
+			<Profound>
+				<Thoughts>
+					<Beliefs>
+						<Profound>
+							<Thoughts>
+								<Beliefs></Beliefs>
+							</Thoughts>
+						</Profound>
+					</Beliefs>
+				</Thoughts>
+			</Profound>
+		</Profound>
+	</Beliefs>
+</Suppose>
+<Beliefs><Profound><Thoughts></Thoughts></Profound></Beliefs>
+
+<Cash>
+	<cache>
+		<Cash></Cash>
+		<cache></cache>
+	</cache>
+	<Cash>
+		<Catch>
+			<cache></cache>
+		</Catch>
+	</Cash>
+</Cash>
+<Cash><cache></cache></Cash>
+
+<toy>
+	<joy></joy>
+	<boy></boy>
+	<soy></soy>
+	<toy>
+		<joy></joy>
+		<boy></boy>
+		<toy>
+			<joy></joy>
+			<boy></boy>
+			<soy></soy>
+		</toy>
+	</toy>
+</toy>
+<toy><soy></soy></toy>
+
+<who>
+	<are>
+		<you>
+			<who></who>
+			<are></are>
+		</you>
+	</are>
+	<who>
+		<am></am>
+		<I></I>
+		<are>
+			<you></you>
+		</are>
+	</who>
+	<whom>
+		<can>
+			<it>
+				<be>
+					<who></who>
+					<are></are>
+					<you></you>
+				</be>
+			</it>
+		</can>
+	</whom>
+</who>
+<who><are><you></you></are></who>
+
+<kisses>
+	<hearts>
+		<x>
+			<o></o>
+		</x>
+		<X></X>
+		<O></O>
+	</hearts>
+	<X></X>
+	<O></O>
+</kisses>
+<X><O></O></X>
+
+<Hola>
+	<Como>
+		<Estas>
+			<Hola>
+				<Como>
+					<Estas></Estas>
+				</Como>
+			</Hola>
+		</Estas>
+	</Como>
+	<Hola>
+		<Como>
+			<Esta>
+				<Usted></Usted>
+			</Esta>
+		</Como>
+	</Hola>
+</Hola>
+<Hola><Como><Estas></Estas></Como></Hola>
+
+<Parent>
+	<Child>
+		<Grandchild>
+			<SuperGrandchild>
+				<EvenYoungerGrandchild>
+					<NeverGonnaSeeGrandchild>
+						<ThisOneDoesntEvenExist></ThisOneDoesntEvenExist>
+					</NeverGonnaSeeGrandchild>
+					<MightSeeThisOne></MightSeeThisOne>
+				</EvenYoungerGrandchild>
+			</SuperGrandchild>
+		</Grandchild>
+	</Child>
+	<FavoriteChild>
+		<Child>
+			<Grandchild>
+				<Dog></Dog>
+			</Grandchild>
+		</Child>
+	</FavoriteChild>
+	<OtherChild>
+		<SomeonesChild>
+			<Child>
+				<Grandchild></Grandchild>
+				<ThisOne></ThisOne>
+			</Child>
+			<EvilTwin></EvilTwin>
+		</SomeonesChild>
+	</OtherChild>
+</Parent>
+<Child><Grandchild></Grandchild></Child>
+
+<happy>
+	<happy>
+		<joy>
+			<joy></joy>
+		</joy>
+	</happy>
+	<birthday></birthday>
+	<Valentines>
+		<day></day>
+	</Valentines>
+	<to>
+		<see>
+			<you>
+				<happy>
+					<happy>
+						<joy></joy>
+					</happy>
+				</happy>
+			</you>
+		</see>
+	</to>
+</happy>
+<happy><happy><joy><joy></joy></joy></happy></happy>
+
+<oct>
+	<libra>
+		<osha>
+			<lids></lids>
+			<king></king>
+		</osha>
+		<lyrics>
+			<hello></hello>
+		</lyrics>
+	</libra>
+</oct>
+<libra><osha></osha></libra>
+
+<words>
+	<dice>
+		<cold></cold>
+		<ice></ice>
+	</dice>
+	<dime>
+		<time></time>
+	</dime>
+</words>
+<dice><ice></ice></dice>
+
+<AAA>
+	<Bbb>
+		<abc></abc>
+	</Bbb>
+	<ccc>
+		<abc></abc>
+		<bbb></bbb>
+	</ccc>
+</AAA>
+<Bbb></Bbb>
+
+<lego>
+	<never>
+		<say>
+			<never></never>
+		</say>
+		<pollo>
+			<loco></loco>
+		</pollo>
+		<face></face>
+	</never>
+</lego>
+<never></never>
+
+<case>
+	<test></test>
+</case>
+<test></test>
+
+<liars>
+	<gonna>
+		<lie></lie>
+	</gonna>
+	<haters>
+		<gonna>
+			<hate></hate>
+		</gonna>
+	</haters>
+</liars>
+<haters><gonna></gonna></haters>
+
+<define>
+	<peace>
+		<war></war>
+	</peace>
+</define>
+<define><peace></peace></define>
+
+<local>
+	<gloves></gloves>
+	<toes>
+		<little></little>
+		<big></big>
+		<fat></fat>
+	</toes>
+	<fat>
+		<faces></faces>
+	</fat>
+</local>
+<local><fat></fat></local>
+
+<bros>
+	<before>
+		<hose></hose>
+	</before>
+	<afore>
+		<beaches></beaches>
+	</afore>
+	<after>
+		<me>
+			<bros>
+				<before></before>
+			</bros>
+		</me>
+	</after>
+</bros>
+<bros><afore></afore></bros>
+
+<butter>
+	<toast>
+		<evil></evil>
+		<maybe></maybe>
+	</toast>
+	<evil></evil>
+	<maybe></maybe>
+</butter>
+<butter><evil></evil><maybe></maybe></butter>
+
+<have>
+	<yourself>
+		<a></a>
+	</yourself>
+	<merry>
+		<little>
+			<christmas></christmas>
+		</little>
+	</merry>
+	<or>
+		<have>
+			<yourself></yourself>
+		</have>
+	</or>
+</have>
+<or><have><yourself></yourself></have></or>
+
+<fu>
+	<man></man>
+	<chu>
+		<man>
+			<chu></chu>
+		</man>
+	</chu>
+</fu>
+<fu><man><chu></chu></man></fu>
+
+<oreos>
+	<chips></chips>
+	<ahoy></ahoy>
+	<lettuce>
+		<oreos>
+			<chips></chips>
+		</oreos>
+	</lettuce>
+</oreos>
+<oreos></oreos>
+
+<lets>
+	<stay>
+		<together></together>
+	</stay>
+</lets>
+<lets><together></together></lets>
+
+<github>
+	<is>
+		<cool></cool>
+	</is>
+	<or>
+		<is></is>
+		<uncool></uncool>
+	</or>
+	<nuts></nuts>
+</github>
+<is></is>
+
+<cs>
+	<noncs>
+		<nonsense></nonsense>
+	</noncs>
+</cs>
+<cs><noncs><nonsense></nonsense></noncs></cs>
+
+<capital>
+	<capitol>
+		<Capital>
+			<Capitol></Capitol>
+		</Capital>
+	</capitol>
+</capital>
+<Capital></Capital>
+
+<do>
+	<re>
+		<mi>
+			<fa>
+				<so>
+					<la>
+						<ti>
+							<do></do>
+						</ti>
+					</la>
+				</so>
+			</fa>
+		</mi>
+	</re>
+</do>
+<do><ti></ti></do>
+
+<create>
+	<delete>
+		<destroy>
+			<create>
+				<delete></delete>
+			</create>
+		</destroy>
+	</delete>
+</create>
+<delete><destroy></destroy></delete>
+
+<qwerty>
+	<lambda>
+		<loci></loci>
+		<lambda>
+			<loci>
+				<lambda>
+					<loci></loci>
+				</lambda>
+			</loci>
+		</lambda>
+	</lambda>
+</qwerty>
+<loci><lambda></lambda></loci>
+
+<lisa>
+	<liza></liza>
+	<missy></missy>
+	<kermit>
+		<sprinkles></sprinkles>
+	</kermit>
+	<lola>
+		<sprinkle></sprinkle>
+	</lola>
+</lisa>
+<lisa><lola></lola></lisa>
+
+<graham>
+	<marshmallow>
+		<chocolate></chocolate>
+		<graham></graham>
+		<marshmallow></marshmallow>
+	</marshmallow>
+</graham>
+<marshmallow></marshmallow>
+
+<one>
+	<zero>
+		<one>
+			<zero></zero>
+		</one>
+	</zero>
+	<one>
+		<zero></zero>
+	</one>
+</one>
+<zero><one></one></zero>
+
+<HOPE>
+	<Hope>
+		<hope></hope>
+		<Hope></Hope>
+		<HOPE></HOPE>
+	</Hope>
+	<hope></hope>
+	<HOPE></HOPE>
+</HOPE>
+<HOPE></HOPE>
+
+<escape>
+	<the>
+		<fate>
+			<pierce></pierce>
+			<the></the>
+			<veil></veil>
+		</fate>
+		<veil></veil>
+	</the>
+</escape>
+<escape><the><fate></fate></the></escape>
+
+<Karma>
+	<Karla>
+		<Karmen></Karmen>
+		<Karen></Karen>
+		<Caramel></Caramel>
+		<Karla></Karla>
+	</Karla>
+</Karma>
+<Carla></Carla>
+
+<Suppose>
+	<Beliefs>
+		<Profound>
+			<Thoughts>
+				<Beliefs></Beliefs>
+			</Thoughts>
+			<Profound>
+				<Thoughts>
+					<Beliefs>
+						<Profound>
+							<Thoughts>
+								<Beliefs></Beliefs>
+							</Thoughts>
+						</Profound>
+					</Beliefs>
+				</Thoughts>
+			</Profound>
+		</Profound>
+	</Beliefs>
+</Suppose>
+<Profound><Thoughts></Thoughts></Profound>
+
+<Cash>
+	<cache>
+		<Cash></Cash>
+		<cache></cache>
+	</cache>
+	<Cash>
+		<Catch>
+			<cache></cache>
+		</Catch>
+	</Cash>
+</Cash>
+<cache><Cash></Cash></cache>
+
+<toy>
+	<joy></joy>
+	<boy></boy>
+	<soy></soy>
+	<toy>
+		<joy></joy>
+		<boy></boy>
+		<toy>
+			<joy></joy>
+			<boy></boy>
+			<soy></soy>
+		</toy>
+	</toy>
+</toy>
+<toy><toy></toy></toy>
+
+<who>
+	<are>
+		<you>
+			<who></who>
+			<are></are>
+		</you>
+	</are>
+	<who>
+		<am></am>
+		<I></I>
+		<are>
+			<you></you>
+		</are>
+	</who>
+	<whom>
+		<can>
+			<it>
+				<be>
+					<who></who>
+					<are></are>
+					<you></you>
+				</be>
+			</it>
+		</can>
+	</whom>
+</who>
+<who></who>
+
+<kisses>
+	<hearts>
+		<x>
+			<o></o>
+		</x>
+		<X></X>
+		<O></O>
+	</hearts>
+	<X></X>
+	<O></O>
+</kisses>
+<x><o></o></x>
+
+<Hola>
+	<Como>
+		<Estas>
+			<Hola>
+				<Como>
+					<Estan></Estan>
+				</Como>
+			</Hola>
+		</Estas>
+	</Como>
+	<Hola>
+		<Como>
+			<Esta>
+				<Usted></Usted>
+			</Esta>
+		</Como>
+	</Hola>
+</Hola>
+<Hola><Como><Estas><Usted></Usted></Estas></Como></Hola>
+
+<Parent>
+	<Child>
+		<Grandchild>
+			<SuperGrandchild>
+				<EvenYoungerGrandchild>
+					<NeverGonnaSeeGrandchild>
+						<ThisOneDoesntEvenExist></ThisOneDoesntEvenExist>
+					</NeverGonnaSeeGrandchild>
+					<MightSeeThisOne></MightSeeThisOne>
+				</EvenYoungerGrandchild>
+			</SuperGrandchild>
+		</Grandchild>
+	</Child>
+	<FavoriteChild>
+		<Child>
+			<Grandchild>
+				<Dog></Dog>
+			</Grandchild>
+		</Child>
+	</FavoriteChild>
+	<OtherChild>
+		<SomeonesChild>
+			<Child>
+				<Grandchild></Grandchild>
+				<ThisOne></ThisOne>
+			</Child>
+			<EvilTwin></EvilTwin>
+		</SomeonesChild>
+	</OtherChild>
+</Parent>
+<FavoriteChild><Child><Grandchild></Grandchild></Child></FavoriteChild><OtherChild></OtherChild>
+
+<happy>
+	<happy>
+		<joy>
+			<joy></joy>
+		</joy>
+	</happy>
+	<birthday></birthday>
+	<Valentines>
+		<day></day>
+	</Valentines>
+	<to>
+		<see>
+			<you>
+				<happy>
+					<happy>
+						<joy></joy>
+					</happy>
+				</happy>
+			</you>
+		</see>
+	</to>
+</happy>
+<happy><joy></joy></happy>
+
+<The>
+	<Prof>
+		<Knows>
+			<Whats>
+				<Up></Up>
+			</Whats>
+		</Knows>
+	</Prof>
+	<TAs>
+		<Also>
+			<Know>
+				<Whats>
+					<Up></Up>
+				</Whats>
+			</Know>
+		</Also>
+	</TAs>
+	<But>
+		<Do>
+			<You>
+				<Know>
+					<Whats>
+						<Up></Up>
+					</Whats>
+				</Know>
+			</You>
+		</Do>
+	</But>
+</The>
+<Know><Whats><Up></Up></Whats></Know>
+
+
+
